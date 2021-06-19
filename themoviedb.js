6f094e52d3738a4d7c71a21e64e890ca8a1f6e75
Rafael <contato@rafael-pereira.com>
2021-06-18 23:03:13 -0300

    Correções no .js

--- themoviedb.js
+++ themoviedb.js
@@ -1,54 +1,51 @@
 
-//TMDB
-
-const api_key = "api_key=fb61640a6949cbe5abf035a7d4b7a269";
-const base_url= "https://api.themoviedb.org/3";
-const images_url= "https://image.tmdb.org/t/p/w500";
-const timeout= 2000;
-const api_url = base_url + "/discover/movie?language=pt-br,sort_by=popularity.desc&"+api_key;
-let xhr;
 //-------------------------------------------------------------------------------------------------------
 
-function carregaFilmes () {
-  xhr = new XMLHttpRequest ();
+const TMDB_ENDPOINT = 'https://api.themoviedb.org/3';
+const APIKEY = 'fb61640a6949cbe5abf035a7d4b7a269';
+const IMG_PREFIX = 'https://image.tmdb.org/t/p/w500';
+let xhr;
 
-  xhr.open ('GET', base_url+ '/movie/popular' + '?language=pt-br,api_key=' + api_key, true);
-  xhr.onload = exibeFilmes;
-  xhr.send();
+function carregaFilmes () {
+    xhr = new XMLHttpRequest ();
+
+    xhr.open ('GET', TMDB_ENDPOINT + '/movie/popular' + '?api_key=' + APIKEY, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
 }
 
 function pesquisaFilmes () {
-  xhr = new XMLHttpRequest ();
+    xhr = new XMLHttpRequest ();
 
-  query = document.getElementById('pesquisa').value;
+    query = document.getElementById('pesquisa').value;
 
-  xhr.open ('GET', base_url + '/search/movie' + '?language=pt-br,api_key=' + api_key + '&query=' + query, true);
-  xhr.onload = exibeFilmes;
-  xhr.send();
+    xhr.open ('GET', TMDB_ENDPOINT + '/search/movie' + '?api_key=' + APIKEY + '&query=' + query, true);
+    xhr.onload = exibeFilmes;
+    xhr.send();
 }
 
 
 function exibeFilmes () {
-  
-  let data = JSON.parse (xhr.responseText);
-  let textoHTML = '';
+    
+    let data = JSON.parse (xhr.responseText);
+    let textoHTML = '';
 
-  for (let i = 0; i < data.results.length; i++) {
-      let nomeFilme = data.results[i].title;
-      let sinopse = data.results[i].overview;
-      let imagem = images_url + data.results[i].poster_path;
+    for (let i = 0; i < data.results.length; i++) {
+        let nomeFilme = data.results[i].title;
+        let sinopse = data.results[i].overview;
+        let imagem = IMG_PREFIX + data.results[i].poster_path;
 
-      textoHTML += `<div class="card col-md-4">
-          <img src="${imagem}" class="card-img-top" alt="...">
-          <div class="card-body">
-              <h5 class="card-title">${nomeFilme}</h5>
-              <p class="card-text">${sinopse}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-      </div>`
-  }
+        textoHTML += `<div class="card col-md-4">
+            <img src="${imagem}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${nomeFilme}</h5>
+                <p class="card-text">${sinopse}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`
+    }
 
-  document.getElementById('tela').innerHTML = textoHTML;
+    document.getElementById('tela').innerHTML = textoHTML;
 }
 
 //-------------------------------------------------------------------------------------------------------
